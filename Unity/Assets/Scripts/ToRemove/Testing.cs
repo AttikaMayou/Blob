402b2a3bb85ccd9beb803add144a39c35a0a60c7
Attika <attika.mayou@gmail.com>
2020-02-23 18:23:57 +0100

    WIP implementation ECS

--- Unity/Assets/Scripts/ToRemove/Testing.cs
+++ Unity/Assets/Scripts/ToRemove/Testing.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections;
+﻿using Unity.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Transforms;
 
 public class Testing : MonoBehaviour
 {
      private void Start()
      {
         EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-        Entity entity = entityManager.CreateEntity(typeof(LevelComponent));
 
-        entityManager.SetComponentData(entity, new LevelComponent { level = 10 });
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(LevelComponent),
+            typeof(Translation)
+        );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(2000, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+
+        for (int i = 0; i < entityArray.Length; ++i)
+        {
+            Entity entity = entityArray[i];
+            entityManager.SetComponentData(entity, new LevelComponent { level = Random.Range(10, 20) });
+        }
+
+        entityArray.Dispose();
      }
 }
