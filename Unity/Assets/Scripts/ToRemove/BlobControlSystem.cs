3e38f24fb2b6b0ef00c9cfe9b3568e38ec555337
Attika <attika.mayou@gmail.com>
2020-02-25 11:19:30 +0100

    WIP inputs

---
+++ Unity/Assets/Scripts/ToRemove/BlobControlSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class BlobControlSystem : ComponentSystem
+{
+    private float3 startPosition;
+
+    protected override void OnUpdate()
+    {
+        if(Input.GetMouseButton(0))
+        {
+
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+
+        }
+
+    }
+}
