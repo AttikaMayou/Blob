5ce150c9a6a4ac749950739d83e468be0ac23adb
Attika <attika.mayou@gmail.com>
2020-03-03 16:56:54 +0100

    Starting customized conversion of gameobject to entity

---
+++ Unity/Assets/Scripts/GameObjectToEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class GameObjectToEntity : MonoBehaviour
+{
+    public GameObject go;
+
+    private Entity entity;
+    private BlobAssetStore blobAssetStore;
+
+    private void Awake()
+    {
+        blobAssetStore = new BlobAssetStore();
+        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, blobAssetStore);
+        entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(go, settings);
+        Destroy(go);
+    }
+
+    private void OnDestroy()
+    {
+        blobAssetStore.Dispose();
+    }
+}
