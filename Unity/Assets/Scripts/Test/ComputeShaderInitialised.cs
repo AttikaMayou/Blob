dd4b6087eb17e461f03b2c547860b6be6c5f9a64
Margot <gotgot07@hotmail.fr>
2020-05-14 15:03:13 +0200

    début test compute shader

---
+++ Unity/Assets/Scripts/Test/ComputeShaderInitialised.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputeShaderInitialised : MonoBehaviour
+{
+    [SerializeField] private ComputeShader myComputeShader;
+    private int indexOfKernel;
+
+    void Start()
+    {
+        indexOfKernel = myComputeShader.FindKernel("CSMain");
+    }
+    void UseShaderAnytime()
+    {
+        int x = 16;
+        int y = 16;
+        int z = 4;
+        myComputeShader.Dispatch(indexOfKernel, x, y, z);
+    }
+}
