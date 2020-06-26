eed0b90746891f3c8c4013f3bdeb0c147ceda0c5
Margot <gotgot07@hotmail.fr>
2020-06-26 12:41:21 +0200

    shadow marche pas

---
+++ Unity/Assets/Scripts/ArthurRaymarching/RaymarchingCameraShadow.cs
@@ -0,0 +1,269 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Utils;
+
+[RequireComponent(typeof(Camera))]
+public class RaymarchingCameraShadow : SceneViewFilter
+{
+
+    [SerializeField]
+    private Shader _shader;
+
+    public Material _raymarchMaterial
+    {
+        get
+        {
+            if (!_raymarchMat && _shader)
+            {
+                _raymarchMat = new Material(_shader);
+                _raymarchMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            return _raymarchMat;
+        }
+    }
+    private Material _raymarchMat;
+
+    public Camera _camera
+    {
+        get
+        {
+            if (!_cam)
+                _cam = GetComponent<Camera>();
+            return _cam;
+        }
+    }
+    private Camera _cam;
+
+    [Header("Camera")]
+    public float _maxDistance;
+    [Range(1, 300)] public int _maxIterations;
+    [Range(0.1f, 0.001f)] public float _accuracy;
+
+    [Header("Objects")]
+    private Vector4[] _spheresPos;
+
+    [Header("Light")]
+    public Light _light;
+
+    [Header("Shadow")]
+    [Range(1, 4)] public float _shadowIntensity;
+    public Vector2 _shadowDistance;
+    [Range(1, 128)] public float _shadowPenumbra;
+
+    [Header("Ambiant occlusion")]
+    [Range(0.01f, 10.0f)] public float _aoStepSize;
+    [Range(1, 5)] public int _aoIterations;
+    [Range(0, 1)] public float _aoIntensity;
+
+    [Header("Reflection")]
+    public Slider reflectionCountSlider;
+    [Range(0, 2)] public int _reflectionCount = 2;
+    [Range(0, 1)] public float _reflectionIntensity;
+    [Range(0, 1)] public float _envReflectionIntensity;
+    public Cubemap _reflectionCube;
+
+    [Header("Color")]
+    public Gradient _sphereGradiant;
+    private Color[] _sphereColor;
+    [Range(0, 4)] public float _colorIntensity;
+
+    [Header("SDF")]
+    public Slider smoothSlider;
+    [Range(0, 10)] public float _sphereSmooth = 0.7f;
+
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!_raymarchMaterial)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        _spheresPos = new Vector4[BlobUtils.GetBlobsCurrentPositions().Count];
+        int nbSphere = BlobUtils.GetBlobsCurrentPositions().Count;
+
+        if (nbSphere > 0)
+        {
+            _sphereColor = new Color[nbSphere];
+
+            for (int i = 0; i < BlobUtils.GetBlobsCurrentPositions().Count; i++)
+            {
+                _spheresPos[i] = BlobUtils.GetBlobsCurrentPositions()[i];
+                _sphereColor[i] = _sphereGradiant.Evaluate(1f / 8 * (i % 8));
+            }
+        }
+        else
+        {
+            _spheresPos = new Vector4[1];
+            _sphereColor = new Color[1];
+        }
+        _spheresPos = new Vector4[1];
+        _spheresPos[0] = new Vector4(0, 0, 0, 1);
+
+        //Set spheres property
+        //if (_spheres.Count == 0)
+        //{
+        //    _spheresPos = new Vector4[8];
+        //    _sphereColor = new Color[8];
+
+        //    for (int i = 0; i < 8; i++)
+        //    {
+        //        Vector3 pos = Vector3.zero;
+        //        _spheresPos[i] = new Vector4(pos.x, pos.y, pos.z, 4.0f);
+        //        _sphereColor[i] = _sphereGradiant.Evaluate(1f / 8 * i);
+        //    }
+        //}
+        //else
+        //{
+
+        //    _spheresPos = new Vector4[_spheres.Count];
+        //    _sphereColor = new Color[_spheres.Count];
+
+        //    for (int i = 0; i < _spheres.Count; i++)
+        //    {
+        //        Vector3 pos = _spheres[i].transform.position;
+        //        _spheresPos[i] = new Vector4(pos.x, pos.y, pos.z, _spheres[i].transform.localScale.x); 
+        //        _sphereColor[i] = _sphereGradiant.Evaluate(1f / 8 * i % 8);
+        //    }
+        //}
+
+        // Camera
+        _raymarchMaterial.SetMatrix("_CamFrustum", GetFrustumCorners(_camera));
+        _raymarchMaterial.SetMatrix("_CamToWorld", _camera.cameraToWorldMatrix);
+        _raymarchMaterial.SetFloat("_maxDistance", _maxDistance);
+        _raymarchMaterial.SetVector("_camPos", this.transform.position);
+        _raymarchMaterial.SetInt("_maxIterations", _maxIterations);
+        _raymarchMaterial.SetFloat("_accuracy", _accuracy);
+
+
+        //Light
+        _raymarchMaterial.SetVector("_lightDir", _light ? _light.transform.forward : Vector3.down);
+        _raymarchMaterial.SetVector("_LightDirectionRight", _light.transform.right.normalized);
+        _raymarchMaterial.SetVector("_LightDirectionUp", _light.transform.up.normalized);
+        _raymarchMaterial.SetColor("_lightCol", _light.color);
+        _raymarchMaterial.SetFloat("_lightIntensity", _light.intensity);
+
+        //Shadows
+        _raymarchMaterial.SetVector("_shadowDistance", _shadowDistance);
+        _raymarchMaterial.SetFloat("_shadowIntensity", _shadowIntensity);
+        _raymarchMaterial.SetFloat("_shadowPenumbra", _shadowPenumbra);
+
+        //Ambiant occlusion
+        _raymarchMaterial.SetFloat("_aoStepSize", _aoStepSize);
+        _raymarchMaterial.SetInt("_aoIterations", _aoIterations);
+        _raymarchMaterial.SetFloat("_aoIntensity", _aoIntensity);
+
+        //Reflection
+        _raymarchMaterial.SetInt("_reflectionCount", _reflectionCount);
+        _raymarchMaterial.SetFloat("_reflectionIntensity", _reflectionIntensity);
+        _raymarchMaterial.SetFloat("_envReflectionIntensity", _envReflectionIntensity);
+        _raymarchMaterial.SetTexture("_reflectionCube", _reflectionCube);
+
+        //Color
+        _raymarchMaterial.SetColorArray("_sphereColor", _sphereColor);
+        _raymarchMaterial.SetFloat("_colorIntensity", _colorIntensity);
+
+        //SDF
+        _raymarchMaterial.SetVectorArray("_spheres", _spheresPos);
+        _raymarchMaterial.SetInt("_nbSphere", nbSphere);
+        _raymarchMaterial.SetFloat("_sphereSmooth", _sphereSmooth);
+
+        CustomGraphicsBlit(source, destination, _raymarchMaterial, 0);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    /// \brief Stores the normalized rays representing the camera frustum in a 4x4 matrix.  Each row is a vector.
+    /// 
+    /// The following rays are stored in each row (in eyespace, not worldspace):
+    /// Top Left corner:     row=0
+    /// Top Right corner:    row=1
+    /// Bottom Right corner: row=2
+    /// Bottom Left corner:  row=3
+    private Matrix4x4 GetFrustumCorners(Camera cam)
+    {
+        float camAspect = cam.aspect;
+
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        float fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
+
+        Vector3 toRight = Vector3.right * fov * camAspect;
+        Vector3 toTop = Vector3.up * fov;
+
+        Vector3 topLeft = (-Vector3.forward - toRight + toTop);
+        Vector3 topRight = (-Vector3.forward + toRight + toTop);
+        Vector3 bottomRight = (-Vector3.forward + toRight - toTop);
+        Vector3 bottomLeft = (-Vector3.forward - toRight - toTop);
+
+        frustumCorners.SetRow(0, topLeft);
+        frustumCorners.SetRow(1, topRight);
+        frustumCorners.SetRow(2, bottomRight);
+        frustumCorners.SetRow(3, bottomLeft);
+
+        return frustumCorners;
+    }
+
+    /// \brief Custom version of Graphics.Blit that encodes frustum corner indices into the input vertices.
+    /// 
+    /// In a shader you can expect the following frustum cornder index information to get passed to the z coordinate:
+    /// Top Left vertex:     z=0, u=0, v=0
+    /// Top Right vertex:    z=1, u=1, v=0
+    /// Bottom Right vertex: z=2, u=1, v=1
+    /// Bottom Left vertex:  z=3, u=1, v=0
+    /// 
+    /// \warning You may need to account for flipped UVs on DirectX machines due to differing UV semantics
+    ///          between OpenGL and DirectX.  Use the shader define UNITY_UV_STARTS_AT_TOP to account for this.
+    static void CustomGraphicsBlit(RenderTexture source, RenderTexture dest, Material fxMaterial, int passNr)
+    {
+        RenderTexture.active = dest;
+
+        fxMaterial.SetTexture("_MainTex", source);
+
+        GL.PushMatrix();
+        GL.LoadOrtho(); // Note: z value of vertices don't make a difference because we are using ortho projection
+
+        fxMaterial.SetPass(passNr);
+
+        GL.Begin(GL.QUADS);
+
+        // Here, GL.MultitexCoord2(0, x, y) assigns the value (x, y) to the TEXCOORD0 slot in the shader.
+        // GL.Vertex3(x,y,z) queues up a vertex at position (x, y, z) to be drawn.  Note that we are storing
+        // our own custom frustum information in the z coordinate.
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    #region UI_FUNCTIONS
+
+    public void UpdateSphereSmooth()
+    {
+        _sphereSmooth = smoothSlider.value;
+    }
+
+    public void UpdateReflectionCount()
+    {
+        _reflectionCount = (int)reflectionCountSlider.value;
+    }
+
+    #endregion
+}
---
+++ Unity/Assets/Shaders/Arthur/RaymarchingShadow.shader
@@ -0,0 +1,325 @@
+﻿Shader "Arthur/RaymarchingShadow"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+		Tags{"LightMode" = "ForwardBase" }
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#pragma target 3.0
+
+            #include "UnityCG.cginc"
+			#include "DistanceFunctions.cginc"
+
+			sampler2D _MainTex;
+
+			//Camera
+			uniform sampler2D _CameraDepthTexture;
+			uniform float4x4 _CamFrustum;
+			uniform float4x4 _CamToWorld;
+			uniform float3 _camPos;
+			uniform float _maxDistance;
+			uniform int _maxIterations;
+			uniform float _accuracy;
+			
+			//Light
+			uniform float3 _lightDir;
+			uniform float3 _lightCol;
+			uniform float _lightIntensity;
+			uniform float3 _LightDirectionRight;
+			uniform float3 _LightDirectionUp;
+
+			//Shadows
+			uniform float2 _shadowDistance;
+			uniform float _shadowIntensity;
+			uniform float _shadowPenumbra;
+
+			//Ambiant occlusion
+			uniform float _aoStepSize;
+			uniform int _aoIterations;
+			uniform float _aoIntensity;
+
+			//Reflection
+			uniform int _reflectionCount;
+			uniform float _reflectionIntensity;
+			uniform float _envReflectionIntensity;
+			uniform samplerCUBE _reflectionCube;
+			
+			//SDF
+			uniform int _nbSphere;
+			uniform float4 _spheres[999];
+			uniform float _sphereSmooth;
+			uniform float _degreRotate;
+			uniform float _rotationSpeed;
+
+			//Color
+			uniform fixed4 _groundColor;
+			uniform fixed4 _sphereColor[999];
+			uniform float _colorIntensity;
+
+			//Shadow From scene
+			uniform sampler2D m_ShadowmapCopy;
+
+			//Input
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+			//Output
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+				float3 ray : TEXCOORD1;
+            };
+			
+			//Vertex
+            v2f vert (appdata v)
+            {
+                v2f o;
+				half index = v.vertex.z;
+				v.vertex.z = 0;
+
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+
+				o.ray = _CamFrustum[(int)index].xyz;
+
+				o.ray /= abs(o.ray.z);
+
+				o.ray = mul(_CamToWorld, o.ray);
+
+                return o;
+            }
+
+			float4 distanceField(float3 p, float depth) 
+			{
+				//float4 distanceScene = float4(_planeColor.rgb, depth - length(p - _camPos));
+
+				//Distance
+				float4 sphereAdd = float4(_sphereColor[0].rgb, sdSphere(p - _spheres[0].xyz, _spheres[0].w));
+				float4 result = sphereAdd;
+
+				for (int i = 1; i < _nbSphere; i++)
+				{
+					//Distance
+					sphereAdd = float4(_sphereColor[i].rgb, sdSphere(p - _spheres[i].xyz, _spheres[i].w));				
+				
+					result = opUS(result, sphereAdd, _sphereSmooth);
+				}
+
+				return result;
+			}
+
+			float3 getNormal(float3 p, float depth) 
+			{
+				const float2 offset = float2(0.001, 0);
+				float3 n = float3(
+					distanceField(p + offset.xyy, depth).w - distanceField(p - offset.xyy, depth).w,
+					distanceField(p + offset.yxy, depth).w - distanceField(p - offset.yxy, depth).w,
+					distanceField(p + offset.yyx, depth).w - distanceField(p - offset.yyx, depth).w);
+
+				return normalize(n);
+			}
+
+			float hardShadow(float3 ro, float3 rd, float minT, float maxT, float depth)
+			{
+				for (float t = minT; t < maxT;)
+				{
+					float h = distanceField(ro + rd * t, depth).w;
+					if (h < 0.001)
+					{
+						return 0.0;
+					}
+
+					t += h;
+				}
+
+				return 1.0;
+			}
+
+			float softShadow(float3 ro, float3 rd, float minT, float maxT, float k, float depth)
+			{
+				float result = 1.0;
+
+				for (float t = minT; t < maxT;)
+				{
+					float h = distanceField(ro + rd * t, depth).w;
+					if (h < 0.001)
+					{
+						return 0.0;
+					}
+					result = min(result, k * h / t);
+
+					t += h;
+				}
+
+				return result;
+			}
+
+			float AmbiantOcclusion(float3 p, float3 n, float depth)
+			{
+				float step = _aoStepSize;
+				float ao = 0.0;
+				float dist;
+
+				for (int i = 1; i <= _aoIterations; i++)
+				{
+					dist = step * i;
+					ao += max(0.0, (dist - distanceField(p + n * dist, depth).w) / dist);
+				}
+
+				return (1.0 - ao * _aoIntensity);
+			}
+
+			float3 Shading(float3 p, float3 n, fixed3 c, float depth) 
+			{
+				float3 result;
+
+				//Diffuse color
+				float3 color = c.rgb * _colorIntensity;
+
+				//Directional light
+				float3 light = (_lightCol * dot(-_lightDir, n) * 0.5 + 0.5) * _lightIntensity;
+
+				////Shadows
+				float shadow = softShadow(p, -_lightDir, _shadowDistance.x, _shadowDistance.y, _shadowPenumbra, depth) * 0.5 + 0.5;
+				shadow = max(0.0, pow(shadow, _shadowIntensity));
+
+				//Ambant occlusion
+				float ao = AmbiantOcclusion(p, n, depth);
+
+				result = color * light * shadow * ao;
+
+				return result;
+			}
+
+			bool raymarching(float3 ro, float3 rd, float depth, float maxDistance, int maxIteration, inout float3 p, inout fixed3 dColor)
+			{
+				bool hit;
+
+				float t = 0; //distance travelled along the ray direction
+
+				for (int i = 0; i < maxIteration; i++)
+				{
+					if (t > maxDistance || t >= depth)
+					{
+						//Environment
+						hit = false;
+						break;
+					}
+
+					p = ro + rd * t;
+
+					//Check for a hit
+					float4 d = distanceField(p, depth);
+					if (d.w < _accuracy) //Hit !
+					{
+						dColor = d.rgb;
+						hit = true;
+						break;
+					}
+
+					t += d.w;
+				}
+
+				return hit;
+			}
+
+			//Fragment
+			fixed4 frag(v2f i) : SV_Target
+			{
+				float depth = LinearEyeDepth(tex2D(_CameraDepthTexture, i.uv).r);
+				depth *= length(i.ray);
+
+				fixed3 col = tex2D(_MainTex, i.uv);
+				float3 rayDirection = normalize(i.ray.xyz);
+				float3 rayOrigin = _WorldSpaceCameraPos;
+				fixed4 result;
+				float3 hitPosition;
+				fixed3 dColor;
+
+				bool hit = raymarching(rayOrigin, rayDirection, depth, _maxDistance, _maxIterations, hitPosition, dColor);
+
+				if (hit)
+				{
+					//Shading
+					float3 n = getNormal(hitPosition, depth);
+					float3 s = Shading(hitPosition, n, dColor, depth);
+					result = fixed4(s, 1);
+					result += fixed4(texCUBE(_reflectionCube, n).rgb * _envReflectionIntensity * _reflectionIntensity, 0);
+
+					//Reflection
+					if (_reflectionCount > 0)
+					{
+						rayDirection = normalize(reflect(rayDirection, n));
+						rayOrigin = hitPosition + (rayDirection * 0.01);
+						hit = raymarching(rayOrigin, rayDirection, _maxDistance, _maxDistance * 0.5, _maxIterations * 0.5, hitPosition, dColor); //Pas de depth pour la reflection
+
+						if (hit)
+						{
+							n = getNormal(hitPosition, depth);
+							s = Shading(hitPosition, n, dColor, depth);
+							result += fixed4(s * _reflectionIntensity, 0);
+
+							if (_reflectionCount > 1)
+							{
+								rayDirection = normalize(reflect(rayDirection, n));
+								rayOrigin = hitPosition + (rayDirection * 0.01);
+								hit = raymarching(rayOrigin, rayDirection, _maxDistance, _maxDistance * 0.25, _maxIterations * 0.25, hitPosition, dColor); //Pas de depth pour la reflection
+
+								if (hit)
+								{
+									n = getNormal(hitPosition, depth);
+									s = Shading(hitPosition, n, dColor, depth);
+									result += fixed4(s * _reflectionIntensity * 0.5, 0);
+								}
+							}
+						}
+					}
+				}
+				else
+				{
+					result = fixed4(0, 0, 0, 0);
+				}
+				
+				//calcul des shadows
+				float4 pInScreenSpace = mul(UNITY_MATRIX_VP, float4(hitPosition.xyz, 1.));
+				depth = pInScreenSpace.z / pInScreenSpace.w;
+
+				float  vDepth = distance(_WorldSpaceCameraPos, float4(hitPosition.xyz, 1.));
+
+				float4 near = float4 (vDepth >= _LightSplitsNear);
+				float4 far = float4 (vDepth < _LightSplitsFar);
+				float4 weights = near * far;
+
+				float3 shadowCoord0 = mul(unity_WorldToShadow[0], float4(hitPosition.xyz, 1.)).xyz;
+				float3 shadowCoord1 = mul(unity_WorldToShadow[1], float4(hitPosition.xyz, 1.)).xyz;
+				float3 shadowCoord2 = mul(unity_WorldToShadow[2], float4(hitPosition.xyz, 1.)).xyz;
+				float3 shadowCoord3 = mul(unity_WorldToShadow[3], float4(hitPosition.xyz, 1.)).xyz; 
+				float3 coord =
+				shadowCoord0 * weights.x + 	// case: Cascaded one
+				shadowCoord1 * weights.y + 	// case: Cascaded two
+				shadowCoord2 * weights.z + 	// case: Cascaded three
+				shadowCoord3 * weights.w; 	// case: Cascaded four
+
+				float shadowmask = tex2D(m_ShadowmapCopy, coord.xy).g;
+				shadowmask = shadowmask < coord.z;
+				col *= max(0.3, shadowmask);
+				return fixed4(col, 1.0);
+				//return fixed4(col * (1.0 - result.w) + result.xyz * result.w ,1.0);
+            }
+            ENDCG
+        }
+    }
+}
